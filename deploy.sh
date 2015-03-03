33bb34efbef6f9ff9311fabaec9a85a19bbef385
Leonardo Giordani <giordani.leonardo@gmail.com>
2015-03-03 09:03:53 +0100

    Categories and code are copied to the previous position too to enable backward compatibility

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,9 @@ pelican content -s publishconf.py
 cd ..
 rm -fR deploy/*
 cp -R pelican/output/* deploy/
+cd deploy
+mkdir download
+cp -R code download/
+cp -R categories blog/
 
 echo "Move to the deploy directory, check, commit and push"
